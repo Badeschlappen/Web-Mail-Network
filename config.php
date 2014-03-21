736603b8357a28d2fd5c76b8697ea09883dd7389
Patrick <bloody1410@web.de>
2014-03-21 16:04:47 +0100

    Badeschlappen
    Datein strukture erstellt

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+// Database connection
+mysql_connect("host", "user", "pass") or die("Error connecting to the database.");
+mysql_select_db("web-mail-network");
+?>                                                                                             
---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <link href="style/style.css" rel="Stylesheet" type="text/css" media="screen"/>
+</head>
+<body>
+    <div id="root">
+        <div id="logo"></div>
+        <div id="subnavi"></div>
+        <div id="navi"></div>
+        <div id="content">
+            <div class="content left"></div>
+            <div class="content right"></div>
+        </div>
+        <div id="footer">
+            <div id="navi-footer"></div>
+        </div>
+    </div>
+</body>
+</html>
